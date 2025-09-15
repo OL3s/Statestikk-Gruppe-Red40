3094c90cd06bdd76df69cb7bd26738ac3bd77042
WilliamHoltsdalen <williamlh@hotmail.no>
2025-09-15 09:32:56 +0200

    feat(src): add pdf output creation

--- forprosjekt/src/main.py
+++ forprosjekt/src/main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 excel_path = Path("../skostr_hoyde.xlsx")
 COL_X = "skostr"
 COL_Y = "hoyde"
+output_pdf_path = Path("../output.pdf")
 
 df = pd.read_excel(excel_path)
 data = df[[COL_X, COL_Y]].dropna().astype(float)
@@ -27,4 +28,7 @@ plt.xlabel(COL_X)
 plt.ylabel(COL_Y)
 plt.legend()
 plt.grid(True)
+
+plt.savefig(output_pdf_path)
+
 plt.show()
